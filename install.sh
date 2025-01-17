079ba92a5c274f56c9043dd45c6466494c49cc9a
jay <jennyguddanti@gmail.com>
2025-01-17 13:47:09 +0200

    Made the shell script actually work

--- install.sh
+++ install.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
+# Define the install function
+function install_func { 
+  cd ~/dotfiles || { echo "Directory ~/dotfiles not found. Exiting."; exit 1; }
+  cd ~/.config
+ rm -rf foot sway waybar rofi
+cd ~/dotfiles
+   mv  rofi sway waybar foot ~/.config
+cd ~/.config
+ls 
+fd
+}
 
 # Ask the user if they want to install required packages
 while true; do
-  read -p "Install required packages for the JDotfiles? (y/n): " choice
+  read -rp "Install required packages for the JDotfiles? (y/n): " choice
 
   case "$choice" in
     y|Y)
-      pacman -S foot  ttf-nerd-fonts-symbols-common ttf-opensans ttf-font-awesome ttf-bistream-vera ttf-dejavu ttf-liberation sway-bg otf-font-awesome wl-roots
-      install
-      echo "Finished install, you can start sway now"
+      sudo pacman -S --needed foot ttf-nerd-fonts-symbols-common ttf-opensans ttf-font-awesome \
+        ttf-dejavu ttf-liberation sway swaybg otf-font-awesome wlroots || { echo "Package installation failed. Exiting."; exit 1; }
+      install_func
       break
       ;;
     n|N)
-      install
+      install_func
       break
       ;;
     *)
@@ -20,10 +31,3 @@ while true; do
       ;;
   esac
 done
-
-# Define the install function
-install() { 
-  git clone https://github.com/coderstarrer/dotfiles.git
-  cd ~/dotfiles
-  mv rofi sway waybar foot  ~/.config/
-}
